62f5bb02936ab771fe7febd234be3beb6e544690
Mateja Verlic <mateja.verlic@gmail.com>
2013-10-16 17:17:38 +0200

    Added advanced example

--- jQuery/2013-08-28-jquery-form-validation/demo/js/myscript.js
+++ jQuery/2013-08-28-jquery-form-validation/demo/js/myscript.js
@@ -36,27 +36,22 @@ jQuery(function($) {
 	
 	$('#specialForm').validate({
 		rules: {
-			demoName: {
-				minlength: 2,
-				required: true
-			},
-			demoEmail: {
-				required: true,
-				email: true
-			},
-			demoPass: {
-				minlength: 2,
-				required: true
-			},
 			optionsGender: "required",
 			checkFunny: {
-				required: true
+				required: true,
+				minlength: 1
 			},
 			selFriends: {
 				required: true
 			},
 			selAttitude: {
 				required: true
+			}
+		},
+		messages :{
+			checkFunny: {
+				required: "Hey! Yo! You ARE funny! Check it now!",
+				minlength: jQuery.format("Please, Check at least one box")
 			}
 		},
 		highlight: function(element) {
@@ -70,6 +65,19 @@ jQuery(function($) {
 			user = $('input[name="demoName"]').val();
 			alert("Yay! " + user + " submitted a valid form!");
 			form.submit();
+		},
+		errorPlacement: function(error, element) {
+		   if (element.attr('type') === 'radio') {
+		      error.insertAfter(
+		          element.siblings('input[type="radio"][name="' + element.attr('name') + '"]:last'));
+		   }
+			else if (element.attr('type') === 'checkbox') {
+				error.insertAfter(
+					element.siblings('input[type="checkbox"][name="' + element.attr('name') + '"]:last'));
+			}
+		   else {
+		      error.insertAfter(element);
+		   }
 		}
 		
 	});
